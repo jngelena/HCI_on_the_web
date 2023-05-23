7998ed132bb18712af759a9650bb2aecdcbe395d
jngelena <jngelena@gmail.com>
2023-05-23 21:26:08 +0200

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'page/chat.dart';
+import 'page/dashboard.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  int currentTab = 0;
+  final List<Widget> screens = [
+    Dashboard(),
+    Chat()
+  ];
+
+  final PageStorageBucket bucket = PageStorageBucket();
+  Widget currentScreen = Dashboard();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageStorage(
+        child: currentScreen,
+        bucket: bucket,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        notchMargin: 10,
+        child: Container(
+          height: 60,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  MaterialButton(
+                    minWidth: 40,
+                    onPressed: () {
+                      setState(() {
+                        currentScreen = Dashboard();
+                        currentTab = 0;
+                      });
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.bluetooth,
+                          color: currentTab == 0 ? Colors.blueAccent : Colors.grey,
+                        )
+                      ],
+                    ),
+                    )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
